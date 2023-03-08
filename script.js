845816476027d5370fb619bafbffe78453135557
Mike <nhj25252@gmail.com>
2023-03-09 06:19:41 +0900

    Set up html grid

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-// What's the point of naming it gameBoard?lol
-const gameBoard = function () {
-  grid: ["", "", "", "", "", "", "", "", ""];
-  console.log(gameBoard);
-};
-
-// constructor for each player object
-// function Player(name, marker) {
-//   this.name = name;
-//   this.marker = marker;
-// }
 // factory to return object
 const Player = (name, marker) => {
   return { name, marker };
@@ -19,3 +8,8 @@ console.log(playerOne);
 
 const playerTwo = Player("Steve", "O");
 console.log(playerTwo);
+
+const gameBoard = (function () {
+  grid: ["", "", "", "", "", "", "", "", ""];
+  console.log(playerOne.marker);
+})();
