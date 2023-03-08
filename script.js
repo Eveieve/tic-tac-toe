70b8e0911f8e3a131e083f4ae9be73b137c6674c
Mike <nhj25252@gmail.com>
2023-03-09 05:38:34 +0900

    Configure eslint

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-//What's the point of naming it gameBoard?lol
+// What's the point of naming it gameBoard?lol
 const gameBoard = function () {
-  grid: ['', '', '', '', '', '', '', '', ''];
+  grid: ["", "", "", "", "", "", "", "", ""];
+  console.log(gameBoard);
 };
-console.log(gameBoard);
-//constructor for each player object
-function Player(name, marker) {
-  this.name = name;
-  this.marker = marker;
-}
 
-// let playerOne = prompt("Who's player 1?");
-// let playerTwo = prompt("Who's player 2?");
-
-const playerOne = new Player('Jim', 'X');
+// constructor for each player object
+// function Player(name, marker) {
+//   this.name = name;
+//   this.marker = marker;
+// }
+// factory to return object
+const Player = (name, marker) => {
+  return { name, marker };
+};
+// Object for each player
+const playerOne = Player("Jim", "X");
 console.log(playerOne);
 
-const playerTwo = new Player('Steve', 'O');
+const playerTwo = Player("Steve", "O");
 console.log(playerTwo);
