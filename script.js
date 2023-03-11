9bee632e7e979195162ed7c260513e1931ce994a
Mike <nhj25252@gmail.com>
2023-03-11 16:15:36 +0900

    Add getMark and addMark methods

--- script.js
+++ script.js
@@ -9,21 +9,37 @@ console.log(playerOne);
 const playerTwo = Player("Steve", "O");
 console.log(playerTwo);
 
-gameboardArr = ["O", "O", "O", "X", "O", "X", "O", "X", "O"];
+gameboard = ["O", "O", "O", "X", "O", "X", "O", "X", "O"];
 
+const displayGameboard = () => gameboard;
+
+let cellMark = 0;
+
+//Things cell has to do
+function cell() {
+  const addMark = (playerMark) => {
+    cellMark = playerMark;
+  };
+  //check if cell is empty
+  const getMark = (cellMark) => cellMark;
+
+  return { addMark, getMark };
+}
 //Set up function to render contents of gameboard array to the page
-function displayGameboard() {
-  const boardDOM = document.querySelector(".grid");
-  gameboardArr.forEach(function (mark) {
-    const markDOM = document.createElement("div");
-    markDOM.textContent = `${mark}`;
-    boardDOM.appendChild(markDOM);
-  });
-}
-displayGameboard();
+// function displayGameboard() {
+//   const boardDOM = document.querySelector(".grid");
+//   gameboardArr.forEach(function (mark, index) {
+//     const markDOM = document.createElement("div");
+//     markDOM.textContent = `${mark}`;
+//     boardDOM.appendChild(markDOM);
+//     console.log(mark, index);
+//   });
+// }
+// displayGameboard();
+//attach an event listener to each cell
+//to turn it to the player's marker
 
-function updateGameboard() {
-  grid.childNodes.remove();
-  gameBoard.forEach(displayGameboard);
-}
-// console.log(grid);
+// function updateGameboard() {
+//   grid.childNodes.remove();
+//   gameBoard.forEach(displayGameboard);
+// }
