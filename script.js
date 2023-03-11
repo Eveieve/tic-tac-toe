8d7030ccfb57195ec8b558a42b843a01e53d6f48
Mike <nhj25252@gmail.com>
2023-03-12 00:43:21 +0900

    [refactor]

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
-const gameboardArr = ["", "", "", "", "", "", "", "", ""];
+const gameboard = (() => {
+  const boardArr = ["", "", "", "", "", "", "", "", ""];
+  const getBoard = () => boardArr;
 
-function Mark(player, mark) {
-  let cell = "";
-  const addMark = (mark) => (cell = mark);
-  const getMark = () => mark;
-  return { addMark, getMark };
-}
-//function to render contents of gameboard array to the page
-function displayGameboard() {
-  const boardDOM = document.querySelector(".grid");
-  gameboardArr.forEach((mark, index) => {
-    const markDOM = document.createElement("div");
-    markDOM.textContent = `${mark}`;
-    boardDOM.appendChild(markDOM);
-    console.log(mark, index);
-  });
-}
+  const addMark = () => {
+    let emptyCell = "";
+    console.log(boardArr);
+    for (let i = 0; i < boardArr.length; i++) {
+      if (boardArr[i] === undefined) {
+        emptyCell = boardArr[i];
+      }
+
+      if (boardArr[i].length === 0) return; //end game
+    }
+    console.log(playerOne.mark);
+    emptyCell = playerOne.mark;
+  };
+
+  return { boardArr, getBoard, addMark };
+})();
+console.log(gameboard.addMark());
+const Player = (name, mark) => {
+  return { name, mark };
+};
+
+const playerOne = Player("One", "O");
+// console.log(playerOne.mark);
+const playerTwo = Player("Two", "X");
 
 //render the updated status of gameboard
 function updateGameboard() {
@@ -30,28 +40,32 @@ function updateGameboard() {
   }
 }
 
-// factory to return object
-const Player = (name, mark) => {
-  return { name, mark };
-};
-// Object for each player
-
 function playRound() {
   let gameCount = 1;
+  GameController.switchTurn;
   addMark();
   updateGameboard();
   gameCount++;
 }
-const playerOne = Player("One", "O");
-
-const playerTwo = Player("Two", "X");
-
+//DOM
+function showGameboard() {
+  const boardDOM = document.querySelector(".grid");
+  boardArr.forEach((mark, index) => {
+    const markDOM = document.createElement("div");
+    markDOM.textContent = `${mark}`;
+    boardDOM.appendChild(markDOM);
+    console.log(mark, index);
+  });
+}
 function GameController() {
   const switchTurn = () => {
     if (gameCount % 2 !== 0) {
-      console.log(`${PlayerOne.name}'s turn!`);
-    } else console.log(`${PlayerTwo.name}'s turn!`);
-
+      console.log(`${playerOne.name}'s turn!`);
+      mark = playerOne.mark;
+    } else {
+      console.log(`${playerTwo.name}'s turn!`);
+      mark = playerTwo.mark;
+    }
     return { switchTurn };
   };
 }
