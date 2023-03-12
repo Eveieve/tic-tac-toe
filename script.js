a446b5a9aca82ad5d22e7fbd3d57e45c00424a21
Mike <nhj25252@gmail.com>
2023-03-13 08:22:03 +0900

    Fix updateGameboard function

--- script.js
+++ script.js
@@ -6,23 +6,24 @@ const playerOne = Player("One", "O");
 const playerTwo = Player("Two", "X");
 
 function Gameboard() {
-  const boardArr = ["O", "X", "", "", "", "", "", "", ""];
+  const boardArr = [".", ".", ".", ".", ".", ".", ".", ".", "."];
   const getBoard = () => boardArr;
 
-  const dropMark = (index) => {
-    boardArr[index] = gameControl.activePlayer.mark; //activePlayer's mark
+  const dropMark = (position) => {
+    boardArr[position] = gameControl.activePlayer.mark; // assign activePlayer's mark
   };
 
-  return { getBoard, dropMark };
+  return { getBoard, dropMark, boardArr };
 }
 const game = Gameboard();
 
 function updateGameboard() {
   const grid = document.querySelector(".grid");
-
-  for (let i = 1; i < grid.childNodes.length; i++) {
-    console.log("remove previous gameboard");
-    grid.childNodes[i].remove();
+  console.log("remove previous gameboard");
+  console.log(grid);
+  console.log(grid.firstChild);
+  while (grid.firstChild) {
+    grid.removeChild(grid.firstChild);
   }
   showGameboard();
 }
@@ -30,30 +31,36 @@ function updateGameboard() {
 function GameController() {
   let activePlayer = playerOne;
 
-  const switchTurn = () =>
-    (activePlayer = activePlayer === playerOne ? playerTwo : playerOne);
-  console.log(activePlayer);
-  const printTurn = () =>
+  const switchTurn = () => {
+    console.log("switch turn!");
+    activePlayer = activePlayer === playerOne ? playerTwo : playerOne;
+    console.log(activePlayer);
+  };
+  const printTurn = () => {
+    const turn = document.querySelector(".turn");
+    console.log(activePlayer);
     activePlayer === playerOne.name
-      ? console.log(`${activePlayer.name}'s turn!`)
-      : console.log(`${activePlayer.name}'s turn!`); //isn't right
+      ? (turn.textContent = `${activePlayer.name}'s turn!`)
+      : (turn.textContent = `${activePlayer.name}'s turn!`);
+  };
   return { switchTurn, printTurn, activePlayer };
 }
 
 const gameControl = GameController();
-console.log(gameControl.activePlayer);
 
 function playRound() {
-  gameControl.printTurn();
   updateGameboard();
-  game.dropMark(playerOne.mark);
+  gameControl.printTurn();
+  game.dropMark();
   gameControl.switchTurn();
 }
 
 playRound();
 playRound();
 //DOM
+
 function showGameboard() {
+  console.log("show current board");
   const boardDOM = document.querySelector(".grid");
   game.getBoard().forEach((mark, index) => {
     const markDOM = document.createElement("div");
